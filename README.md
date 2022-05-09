cfe3c438334583251c43aa309fe40588b9f6d083
valegallo12 <gallov734@gmail.com>
2022-05-09 16:23:38 -0500

    Instruccion repetitiva for

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 ## 2 . capturar un  numero e imprimir el dia de la semana a que corresponde
 
 ## 3 . capturar un numero e imprimir todos los numeros pares que existen entre 1 y el numero digitado
+
+## 4. capturar un numero e imprimir todos los numeros impares que existen entre 1 y el numero digitado en una caja de texto
---
+++ js/codigo4_js.js
@@ -0,0 +1,19 @@
+let impar = "";
+let i = 1;
+
+function ciclowhile(objeto)
+{
+    let numero = parseInt(document.formulario.caja.value);
+    if(Number.isInteger(numero))
+    while(i<=numero)
+    {
+        if(i%2!==0)
+        {
+            impar = impar + i + ",";
+        }
+        i = i+1;
+
+    }
+    document.formulario.respuesta.value = impar;
+    
+} 
